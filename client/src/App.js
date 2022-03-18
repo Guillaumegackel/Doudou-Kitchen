56b3f4c5e6fd1da60a2d8f999dc3753c86c110b3
Webme <guillaume.gackel@gmail.com>
2022-03-18 11:54:03 +0100

    structure front

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import "./App.css";
+import doudou from "./images/doudou.jpg";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
-    <div className="App">
-<h1>DOUDOU KITCHEN</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          DOUDOU KITCHEN
+        </Typography>
+        <img src={doudou} alt="doudou kitchen" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Post from './Post/Post'
+
+function Posts() {
+  return (
+	<>
+	<div>Posts</div>
+	<Post/>
+	<Post/>
+	<Post/>
+	<Post/>
+	<Post/>
+	</>
+  )
+}
+
+export default Posts
