ac107ce76e294d8ad2ee4b63b01e41684499846b
Webme <guillaume.gackel@gmail.com>
2022-03-21 01:57:33 +0100

    Delete Card ok

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,6 +3,9 @@ export default (posts = [], action) => {
     case 'UPDATE':
       return posts.map((post)=> (post._id === action.payload._id ? action.payload : post));
 
+    case 'DELETE':
+      return posts.filter((post)=> (post._id !== action.payload));
+
     case 'FETCH_ALL':
       return action.payload;
 
