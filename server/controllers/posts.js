2c7113d9d230fe1e51b582e1e2e90a1e27fc7b01
Webme <guillaume.gackel@gmail.com>
2022-03-21 02:09:42 +0100

    update delete like route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import PostRecipe from "../models/postRecipe.js";
 
@@ -26,4 +27,41 @@ res.status(409).json({message: error.message})
 }
 
 };
+
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = await PostRecipe.findByIdAndUpdate(_id, {...post,_id}, { new: true });
+
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with id`);
+
+ await PostRecipe.findByIdAndRemove(_id);
+
+ console.log('delete');
+
+  res.json({ message:'Recette supprimée avec succès'});
+};
+
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with id:`);
+
+  const Post = await PostRecipe.findById(_id);
+  const updatedPost = await PostRecipe.findByIdAndUpdate(_id, {likeCount: Post.likeCount +1 }, { new: true });
+
+  res.json(updatedPost);
+};
+
+
 
