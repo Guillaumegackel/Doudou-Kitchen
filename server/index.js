822734df9eb9e635d0670e116f032a3fe65a62f8
Webme <guillaume.gackel@gmail.com>
2022-03-22 22:22:07 +0100

    front back ok

--- server/index.js
+++ server/index.js
@@ -15,14 +15,16 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+const PORT = process.env.PORT;
 
 // utilisation de MongoDB en cloud
 
 // const CONNECTION_URL ='mongodb+srv://guillaume:guillaume123@cluster0.v0hlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`))).catch((error) => console.log(error.message));
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
 
 // mongoose.set('useFindAndModify', false);
 
