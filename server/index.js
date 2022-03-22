a219f318780fd92ecc39db99cf30080d62b40d10
Webme <guillaume.gackel@gmail.com>
2022-03-22 22:39:12 +0100

    server ok

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res)=>{
+	res.send('Hello to the Doudou Kitchen API')
+})
+
 const PORT = process.env.PORT;
 
 // utilisation de MongoDB en cloud
